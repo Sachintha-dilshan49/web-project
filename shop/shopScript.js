74522c1747484f7af49bf77e5b1bbaac9cb5ee5c
Poorna Rashmika <poornarashmika7@gmail.com>
2025-05-15 03:38:45 +0530

    pre-order button was setted

--- shop/shopScript.js
+++ shop/shopScript.js
@@ -49,6 +49,9 @@ preOrderTabBtn.addEventListener('click',()=>{
 });
 
 
+//minus and plus button on pre order
+
+
 //functionality for review tab
 
 const addRevBtn = document.getElementById("addReview");
@@ -95,14 +98,119 @@ document.getElementById('reviewSubmitBtn').addEventListener('click',(event)=>{
 
 //Add to Order
 
+const preOrderItemsNo = document.getElementById('preOrderItems');
+let numOfOrderedItem=0;
 
-const addToOrderBtns = document.querySelectorAll('add-order-btn');
+function preOrderItemNumShow (){
+  if(preOrderItemsNo.textContent>0){
+    preOrderItemsNo.classList.remove('non-visible');
+    preOrderItemsNo.classList.add('visible');
+  }else{
+    preOrderItemsNo.classList.remove('visible');
+    preOrderItemsNo.classList.add('non-visible');
+  }
+}
+preOrderItemNumShow();
+
+function addPreOrdertoList(itemId,itemName,itemPrice){
+
+  const orderItem = document.createElement('div');
+  orderItem.className = "ordered-item";
+  orderItem.id = itemId;
+
+  const orderItemName = document.createElement('div');
+  orderItemName.className = "ordered-item-name";
+  orderItem.textContent = itemName;
+
+  const orderItemDetail = document.createElement('div');
+  orderItemDetail.className = "ordered-item-details";
+
+  const orderItemPrice = document.createElement('span');
+  orderItemPrice.className = "ordered-item-price";
+  orderItemPrice.textContent = itemPrice;
+
+  const orderItemQuantity = document.createElement('span');
+  orderItemQuantity.className = "ordered-item-quantity";
+
+  const orderQuantityMinus = document.createElement('span');
+  orderQuantityMinus.className = 'order-quantity-changer minus';
+  orderQuantityMinus.textContent='-';
+
+  const inputNum = document.createElement('input');
+  inputNum.className = "order-quantity";
+  inputNum.type = "number";
+  inputNum.value=1;
+
+  const orderQuantityPlus = document.createElement('span');
+  orderQuantityPlus.className = "order-quantity-changer plus";
+  orderQuantityPlus.textContent='+';
+
+  orderItemQuantity.appendChild(orderQuantityMinus);
+  orderItemQuantity.appendChild(inputNum);
+  orderItemQuantity.appendChild(orderQuantityPlus);
+  orderItemDetail.appendChild(orderItemPrice);
+  orderItemDetail.appendChild(orderItemQuantity);
+  orderItem.appendChild(orderItemName);
+  orderItem.appendChild(orderItemDetail);
+
+  const orderedItemList = document.getElementById('orderItemList');
+  orderedItemList.appendChild(orderItem);
+}
+
+const addToOrderBtns = document.querySelectorAll('.add-order-btn');
 addToOrderBtns.forEach(addToOrderBtn =>{
   addToOrderBtn.addEventListener('click',()=>{
-    const itemName = addToOrderBtn.previousElementSibling.previousElementSibling.previousElementSibling;
-    
+    numOfOrderedItem++;
+    const itemName = addToOrderBtn.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
+    const itemPrice = addToOrderBtn.previousElementSibling.previousElementSibling.textContent;
+    console.log(itemName);
+    console.log(itemPrice);
+    console.log(addToOrderBtn.parentNode.id);
+
+    addPreOrdertoList(addToOrderBtn.parentNode.id,itemName,itemPrice);
+
+    preOrderItemsNo.textContent++;
+    console.log(preOrderItemsNo.textContent);
+    preOrderItemNumShow();
+
+    refreshNeeds();
+
   })
 }) 
+
+
+
+
+//preOrderSide
+
+// const preOrderInputs = document.querySelectorAll('.order-quantity');
+// preOrderInputs.forEach(preOrderInput=>{
+//   preOrderInput.value=0;
+// })
+OrderPlusBtns = document.querySelectorAll('.order-quantity-changer') && document.querySelectorAll('.plus');
+OrderMinusBtns = document.querySelectorAll('.order-quantity-changer') && document.querySelectorAll('.minus');
+
+function refreshNeeds(){
+  let OrderPlusBtns = document.querySelectorAll('.order-quantity-changer') && document.querySelectorAll('.plus');
+  let OrderMinusBtns = document.querySelectorAll('.order-quantity-changer') && document.querySelectorAll('.minus');
+
+  OrderPlusBtns.forEach(OrderPlusBtn =>{
+    OrderPlusBtn.addEventListener('click',()=>{
+      const orderQuantity = OrderPlusBtn.previousElementSibling;
+      orderQuantity.value++;
+    })
+  });
+
+  OrderMinusBtns.forEach(OrderMinusBtn =>{
+    OrderMinusBtn.addEventListener('click',()=>{
+      const orderQuantity = OrderMinusBtn.nextElementSibling;
+      if(orderQuantity.value>0){
+        orderQuantity.value--;
+      }
+    })
+  });
+}
+
 
 
 
